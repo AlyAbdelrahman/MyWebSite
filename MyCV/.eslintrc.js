0a4bd2688e48f00fbc3e411f3484bda3ca4ae051
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2021-03-07 23:19:21 +0200

    setup Arch + slide component adj

--- MyCV/.eslintrc.js
+++ MyCV/.eslintrc.js
@@ -4,7 +4,28 @@ module.exports = {
     es6: true,
   },
   parser: 'babel-eslint',
-  extends: ['airbnb-base', 'plugin:react/recommended'],
+  settings: {
+    "import/resolver": {
+      node: {
+        moduleDirectory: [
+          "node_modules",
+          "src"
+        ]
+      }
+    },
+    "react": {
+      createClass: "createReactClass", // Regex for Component Factory to use,
+                                         // default to "createReactClass"
+      pragma: "React",  // Pragma to use, default to "React"
+      fragment: "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+      version: "detect", // React version. "detect" automatically picks the version you have installed.
+                           // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+                           // default to latest and warns if missing
+                           // It will default to "detect" in the future
+      flowVersion: "0.53" // Flow version
+    },
+  },
+  extends: ['airbnb-base',"plugin:react/recommended"],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -17,7 +38,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['import','react'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'linebreak-style': 0,
