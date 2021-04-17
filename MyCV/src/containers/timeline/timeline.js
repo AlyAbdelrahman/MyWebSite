5ee81a4fc6a68aa8dffd928832b82c484b66a51a
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2021-04-18 00:22:55 +0200

    adj timline

--- MyCV/src/containers/timeline/timeline.js
+++ MyCV/src/containers/timeline/timeline.js
@@ -16,48 +16,48 @@ const Timeline = ({ setisBackGroundAnimationActive }) => {
                 contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
                 date="2011 - present"
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                icon={<Icon icon="linkedin" size={26.4} className="header__socialMedia__icon" color='white' />}
+                icon={<Icon icon="briefcase" size={26.4} className="header__socialMedia__icon" color='white' />}
             >
-                <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                <h3 className="vertical-timeline-element-title">Cegedim</h3>
                 <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
                 <p>
-                    Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+                Worked as Front-end closely with our development and product team to develop, modify a web-based application and user interfaces across all channels
     </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2010 - 2011"
+                date="2019 - 2020"
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                icon={<Icon icon="linkedin" size={26.4} className="header__socialMedia__icon" color='white' />}
+                icon={<Icon icon="briefcase" size={26.4} className="header__socialMedia__icon" color='white' />}
             >
-                <h3 className="vertical-timeline-element-title">Art Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                <h3 className="vertical-timeline-element-title">Link Development</h3>
+                <h4 className="vertical-timeline-element-subtitle">Maadi, Cairo</h4>
                 <p>
-                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                  Worked As Front-End Developer closely with the design team and development teams to create elegant, usable, responsive, and interactive interfaces across multiple devices
     </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2010 - 2011"
+                date="2018 - 2019"
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                icon={<Icon icon="linkedin" size={26.4} className="header__socialMedia__icon" color='white' />}
+                icon={<Icon icon="graduation-cap" size={26.4} className="header__socialMedia__icon" color='white' />}
             >
-                <h3 className="vertical-timeline-element-title">Art Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                <h3 className="vertical-timeline-element-title">Information Technology Institute</h3>
+                <h4 className="vertical-timeline-element-subtitle">ITI , smart Village</h4>
                 <p>
-                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                   Got accepted in the 9 months program in the internship of "ITI" department of " web developing & User interface"
     </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2010 - 2011"
+                date="2013 - 2018"
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                icon={<Icon icon="linkedin" size={26.4} className="header__socialMedia__icon" color='white' />}
+                icon={<Icon icon="bank" size={26.4} className="header__socialMedia__icon" color='white' />}
             >
-                <h3 className="vertical-timeline-element-title">Art Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                <h3 className="vertical-timeline-element-title">Modern academy for Engineering & Technonolgy</h3>
+                <h4 className="vertical-timeline-element-subtitle">Maddi, Cairo</h4>
                 <p>
-                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                    Studied computer engineering combined with  the fields of electrical engineering and computer science to create new computer hardware and software
     </p>
             </VerticalTimelineElement>
         </VerticalTimeline >
